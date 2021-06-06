c1b91d14ffd7dfc37edc5e43a7d9b9094c924f5f
haris rifai <garmen.yk@gmail.com>
2021-06-06 08:13:18 +0700

    master

--- README.m
+++ README.m
@@ -1,1 +1,2 @@
 # laravel8-adminlte-usercrud
+# Commit minggu 5 june 2021
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'AdminLTE')
+@section('title', 'Dashboard')
 
 @section('content_header')
     <h1 class="m-0 text-dark">Dashboard</h1>
@@ -12,6 +12,16 @@
             <div class="card">
                 <div class="card-body">
                     <p class="mb-0">You are logged in!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0"> </p>
                 </div>
             </div>
         </div>
