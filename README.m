48419cf0c2d5c07cd516555ff8a51f17c97a2322
mac0285 <moh.h.rifai@gmail.com>
2021-06-08 18:01:08 +0700

    master

--- README.m
+++ README.m
@@ -1,2 +1,15 @@
 # laravel8-adminlte-usercrud
 # Commit minggu 5 june 2021
+#git clone git@github.com:akgyk2020/laravel8-adminlte-usercrud.git
+#npm install
+
+#composer install
+#php artisan key:generate
+#php artisan cache:clear
+#php artisan view:clear
+#php artisan config:clear
+
+
+#php artisan migrate
+#php artisan db:seed
+#php artisan migrate:fresh --seed
--- config/database.php
+++ config/database.php
@@ -62,14 +62,13 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '192.168.42.10'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'postgres'),
+            'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
